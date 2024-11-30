0a9fb173a17a5332a03c52663fe50b1f4bed6e6a
Joshua Kim <bki42@uclive.ac.nz>
2024-12-01 09:43:37 +1300

    removed validation

--- schemaTypes/emailRequest.ts
+++ schemaTypes/emailRequest.ts
@@ -15,12 +15,6 @@ export const emailRequest = defineType({
       name: 'adults',
       title: 'Adult',
       type: 'string',
-      validation: Rule => Rule.custom((adults) => {
-        if (!adults || parseInt(adults) <= 0) {
-          return 'There should be at least one adult'
-        }
-        return true
-      })
     }),
     defineField({
       name: 'children',
@@ -31,16 +25,6 @@ export const emailRequest = defineType({
       name: 'clientEmail',
       title: 'Client Email',
       type: 'string',
-      validation: Rule => Rule.custom((email) => {
-        if (!email || email === "safari101@tour.com") {
-          return 'Valid email is required'
-        }
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-        if (!emailRegex.test(email)) {
-          return 'Please enter a valid email'
-        }
-        return true
-      })
     }),
     defineField({
       name: 'arrivalDate',
@@ -51,12 +35,6 @@ export const emailRequest = defineType({
       name: 'description',
       title: 'Description',
       type: 'string',
-      validation: Rule => Rule.custom((description) => {
-        if (!description || description === "Please describe your plan!") {
-          return 'Please describe your trip for a request'
-        }
-        return true
-      })
     }),
     defineField({
       name: 'selectedOptions',
